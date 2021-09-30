366dc87c3bee51b81d7204abc51a6c0c2e81f390
Damjan Vucina <vucinadamjan@gmail.com>
2021-09-30 17:49:59 +0200

    Add basic react native navigation

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-/**
- * @format
- */
+import {Navigation} from 'react-native-navigation';
+import {HomeScreen} from './src/fragments/HomeScreen';
+Navigation.registerComponent('HomeScreen', () => HomeScreen);
 
-import {AppRegistry} from 'react-native';
-import {App} from './src';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'HomeScreen',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
---
+++ src/fragments/HomeScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const HomeScreen: React.FC = React.memo(() => {
+  return (
+    <View>
+      <Text>pero</Text>
+    </View>
+  );
+});

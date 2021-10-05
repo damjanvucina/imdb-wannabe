a1c6aabae8f11f78ab99f592b79485007afc6982
Damjan Vucina <vucinadamjan@gmail.com>
2021-10-05 15:23:44 +0200

    Add search icon

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,4 @@ export {default as HomeTabInactive} from './home-tab-inactive.png';
 export {default as FavoritesTabActive} from './favorites-tab-active.png';
 export {default as FavoritesTabInactive} from './favorites-tab-inactive.png';
 export {default as Logo} from './logo.svg';
+export {default as Search} from './search.svg';
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import {TextInput} from 'react-native';
 import styled from 'styled-components/native';
+import {Search} from '../../assets';
 import {colors} from '../../theme';
 
-export const StyledTextInputPrimary = styled(TextInput)`
+const StyledTextInputPrimary = styled(TextInput)`
   background-color: ${colors.inputBackground};
   border-radius: 10px;
   height: 43px;
   color: ${colors.primary};
   margin: 12px;
-  padding: 10px;
+  padding: 10px 10px 10px 40px;
+  flex: 1;
+`;
+
+const StyledImageContainerPrepended = styled.View`
+  width: 21px;
+  position: absolute;
+  margin-top: 20px;
+  margin-left: 20px; ;
+`;
+
+const StyledViewContainer = styled.View`
+  flex-direction: row;
 `;
 
 export const SearchBar: React.FC = React.memo(() => {
   return (
-    <StyledTextInputPrimary onChangeText={() => {}} placeholder="Search" />
+    <StyledViewContainer>
+      <StyledTextInputPrimary onChangeText={() => {}} placeholder="Search" />
+      <StyledImageContainerPrepended>
+        <Search />
+      </StyledImageContainerPrepended>
+    </StyledViewContainer>
   );
 });

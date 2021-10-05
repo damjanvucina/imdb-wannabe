36a7c7326fdf7cfd4c5794ecde6998e3bd83349f
Damjan Vucina <vucinadamjan@gmail.com>
2021-10-05 15:09:24 +0200

    Move styled components to tsx files

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import {StyledTextInput} from './styled';
+import {TextInput} from 'react-native';
+import styled from 'styled-components/native';
+import {colors} from '../../theme';
+
+export const StyledTextInputPrimary = styled(TextInput)`
+  background-color: ${colors.inputBackground};
+  border-radius: 10px;
+  height: 43px;
+  color: ${colors.primary};
+  margin: 12px;
+  padding: 10px;
+`;
 
 export const SearchBar: React.FC = React.memo(() => {
-  return <StyledTextInput onChangeText={() => {}} placeholder="Search" />;
+  return (
+    <StyledTextInputPrimary onChangeText={() => {}} placeholder="Search" />
+  );
 });

522ab4ab52ef292e838a175b91adf3bd7b1864b1
Damjan Vucina <vucinadamjan@gmail.com>
2021-10-08 13:24:38 +0200

    Make minor style updates to movie category

--- src/components/MovieCategory/index.tsx
+++ src/components/MovieCategory/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import {FlatList, View} from 'react-native';
+import {FlatList, View, StyleSheet} from 'react-native';
 import {Movie} from '../../const';
 import {Title} from '../../theme';
 import {MovieCard} from '../MovieCard';
 
 const StyledTitle = styled(Title)`
   padding-bottom: 8px;
+  padding-right: 18px;
+  padding-left: 18px;
+`;
+
+const CategoryContainer = styled(View)`
+  margin-top: 18px;
+  margin-bottom: 18px;
 `;
 
 const MovieCardSeparator = styled(View)`
@@ -20,7 +27,7 @@ type Props = {
 
 export const MovieCategory: React.FC<Props> = React.memo(({title, movies}) => {
   return (
-    <View>
+    <CategoryContainer>
       <StyledTitle>{title}</StyledTitle>
       <FlatList
         data={movies}
@@ -28,7 +35,15 @@ export const MovieCategory: React.FC<Props> = React.memo(({title, movies}) => {
         ItemSeparatorComponent={MovieCardSeparator}
         keyExtractor={movie => `${movie.id}`}
         horizontal
+        contentContainerStyle={styles.flatListContentContainer}
       />
-    </View>
+    </CategoryContainer>
   );
+});
+
+const styles = StyleSheet.create({
+  flatListContentContainer: {
+    paddingLeft: 18,
+    paddingRight: 18,
+  },
 });

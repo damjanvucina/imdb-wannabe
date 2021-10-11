b1595548ce97c487378485056f8f49ad3a99f621
Damjan Vucina <vucinadamjan@gmail.com>
2021-10-11 11:30:41 +0200

    Show movie preview photo on details screen

--- src/fragments/DetailsScreen/index.tsx
+++ src/fragments/DetailsScreen/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import styled from 'styled-components';
+import {Image} from 'react-native';
 import {NavigationFunctionComponent} from 'react-native-navigation';
 import {ScreenWrapper} from '../../components/ScreenWrapper';
 import {Movie} from '../../const';
+import {getMovieUri} from '../../services';
 
 type Props = {
   movie: Movie;
 };
+
+export const StyledImage = styled(Image)`
+  height: 303px;
+`;
 
 export const DetailsScreen: NavigationFunctionComponent<Props> = React.memo(
   ({componentId: navigationStackId, movie}) => {
     console.log('name je ', movie);
     return (
       <ScreenWrapper navigationStackId={navigationStackId}>
-        <Text>{movie.name}</Text>
+        <StyledImage
+          source={{
+            uri: getMovieUri(movie),
+          }}
+        ></StyledImage>
       </ScreenWrapper>
     );
   },
